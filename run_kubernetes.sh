1db5403fa311fc2ec5903aeafea04b5a3299d757
marybngozi <marybngozi@gmail.com>
2023-03-13 10:19:12 +0100

    add docker and kuber

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=marybngozi/mlkubeapi
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl get nodes 
+
+kubectl create deploy mlkubeapi --image=$dockerpath
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get deploy,rs,svc,pods
 
 # Step 4:
 # Forward the container port to a host
-
+# kubectl port-forward pod/mlkubeapi-868b6f5f4f-552r2 --address 0.0.0.0 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,7 +5,7 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=marybngozi/mlkubeapi
 
 # Step 2:  
 # Authenticate & tag
@@ -13,3 +13,10 @@ echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker tag mlkubeapi $dockerpath
+
+docker images
+
+docker login
+
+docker push $dockerpath
